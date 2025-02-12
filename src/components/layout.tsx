b31d7238850e78b40e90384105a7294d4ece286a
masudursourav <it19025@mbstu.ac.bd>
2025-02-13 01:13:36 +0600

    theme provider added

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,11 +1,13 @@
+import { ThemeProvider } from "@/contex/theme-provider";
+
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="bg-gradient-to-br from-background to-muted">
       header
       <main className="min-h-screen container mx-auto px-4 py-8">
-        {children}
+        <ThemeProvider defaultTheme="dark">{children}</ThemeProvider>
       </main>
-      <footer className="border-t backdrop-blur">
+      <footer className="border-t-4 backdrop-blur supports-[backdrop-filter]:bg-background/60">
         <div className="container mx-auto px-4 py-8 text-center text-gray-400">
           <p>Made By MasudurSourav {new Date().getFullYear()}</p>
         </div>
