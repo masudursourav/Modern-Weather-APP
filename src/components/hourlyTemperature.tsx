92fc52e338d34abadf84990f2e7f4e5a2826ef46
masudursourav <it19025@mbstu.ac.bd>
2025-03-11 00:13:55 +0600

    hourly details component implemented

--- src/components/hourlyTemperature.tsx
+++ src/components/hourlyTemperature.tsx
@@ -1,13 +1,94 @@
 import { ForecastData } from "@/api/types";
+import { format } from "date-fns";
+import {
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 
 function HourlyTemperature({ data }: { data: ForecastData }) {
+  const hourlyForecast = data.list;
+  const chartData = hourlyForecast.slice(0, 8).map((item) => ({
+    time: format(new Date(item.dt * 1000), "ha"),
+    temp: Math.round(item.main.temp),
+    feels_like: Math.round(item.main.feels_like),
+  }));
+
   return (
     <Card className="flex-1">
       <CardHeader>
         <CardTitle>Hourly Temperature</CardTitle>
       </CardHeader>
-      <CardContent></CardContent>
+      <CardContent>
+        <div className="h-[200px] w-full">
+          <ResponsiveContainer width={"100%"} height={"100%"}>
+            <LineChart data={chartData}>
+              <XAxis
+                dataKey="time"
+                stroke="#f4ebeb"
+                fontSize={12}
+                tickLine={false}
+                axisLine={false}
+              />
+              <YAxis
+                stroke="#f4ebeb"
+                fontSize={12}
+                tickLine={false}
+                axisLine={false}
+                tickFormatter={(value) => `${value}°`}
+              />
+              <Tooltip
+                content={({ active, payload }) => {
+                  if (active && payload && payload.length) {
+                    return (
+                      <div className="rounded-lg border  bg-background p-2 shadow-md">
+                        <div className="grid grid-cols-2 gap-2">
+                          <div className="flex flex-col">
+                            <span className="text-[0.7rem uppercase text-muted-foreground">
+                              Temperature:{" "}
+                            </span>
+                            <span className="font-bold">
+                              {payload[0].value}°
+                            </span>
+                          </div>
+                          <div className="flex flex-col">
+                            <span className="text-[0.7rem uppercase text-muted-foreground">
+                              Feels Like:{" "}
+                            </span>
+                            <span className="font-bold">
+                              {payload[1].value}°
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  }
+                  return <div />;
+                }}
+              />
+              <Line
+                type="monotone"
+                dataKey="temp"
+                stroke="#a6a2f3"
+                strokeWidth={2}
+                dot={false}
+              />
+              <Line
+                type="monotone"
+                dataKey="feels_like"
+                stroke="#a6a2f3"
+                strokeWidth={2}
+                strokeDasharray="5 5"
+                dot={false}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+      </CardContent>
     </Card>
   );
 }
